95843e942356231df985b1980900c946ef2c5893
20024498 <20024498@studenti.uniupo.it>
2020-02-12 16:20:17 +0100

    scappo

--- src/model/test/HumanPlayerTest.java
+++ src/model/test/HumanPlayerTest.java
@@ -2,13 +2,64 @@ package model.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import model.HumanPlayer;
+import model.ShipOrientation;
 
 public class HumanPlayerTest {
 
+	HumanPlayer player;
+	
+	@Before
+	public void testInit() {
+		player = new HumanPlayer();
+	}
+	
 	@Test
-	public void test() {
+	public void testSwitchOrientation() {
+		assertEquals(player.getChoiceOrientation(),ShipOrientation.VERTICALE);
+		player.switchOrientation();
+		assertEquals(player.getChoiceOrientation(),ShipOrientation.ORIZZONTALE);
+		
+	}
+
+	@Test
+	public void testShipPositioning() {
+		
+	}
+
+	@Test
+	public void testLecitPosition() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testIsShipOnCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetShipOnCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHitOwnShip() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHitOppGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHitOwnGrid() {
+		fail("Not yet implemented");
+	}
+	
+	
 
 }
--- src/model/test/ShipTest.java
+++ src/model/test/ShipTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import model.Ship;
 import model.ShipCell;
 import model.ShipOrientation;
+import model.ShipState;
 import model.ShipType;
 
 public class ShipTest {
@@ -19,7 +20,7 @@ public class ShipTest {
 	@Before
 	public void init() {
 		
-		ship = new Ship(ShipType.PORTAEREI);
+		ship = new Ship(ShipType.CACCIATORPEDINIERE);
 		ship.setPosition(new ShipCell(0,0), ShipOrientation.ORIZZONTALE);
 		
 	}
@@ -28,22 +29,25 @@ public class ShipTest {
 	public void testSetPosition() {
 		assertEquals(ship.getCells().get(new Point(0,0)) , new ShipCell(0,0));
 		assertEquals(ship.getCells().get(new Point(1,0)) , new ShipCell(1,0));
-		assertEquals(ship.getCells().get(new Point(2,0)) , new ShipCell(2,0));
-		assertEquals(ship.getCells().get(new Point(3,0)) , new ShipCell(3,0));
-		assertEquals(ship.getCells().get(new Point(4,0)) , new ShipCell(4,0));
+		
 	}
 	
 	@Test
 	public void testIsHit() {
+		assertFalse(ship.isHit(0, 0));
 		assertFalse(ship.isHit(1, 0));
 	}
 	
 	@Test
 	public void testHit() {
+		assertFalse(ship.isHit(0, 0));
 		assertFalse(ship.isHit(1, 0));
+		assertTrue(ship.hit(0, 0));
+		assertTrue(ship.isHit(0, 0));
+		assertEquals(ship.getState(),ShipState.COLPITA);
 		assertTrue(ship.hit(1, 0));
 		assertTrue(ship.isHit(1, 0));
-		
+		assertEquals(ship.getState(),ShipState.AFFONDATA);	
 		
 	}
 
