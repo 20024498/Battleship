f28ed81fc39b7b96c1610b96e80eb8746d1e0300
20024498 <20024498@studenti.uniupo.it>
2020-02-13 22:24:40 +0100

    andre 22.30

--- src/model/test/ShipMapTest.java
+++ src/model/test/ShipMapTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.awt.Point;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
